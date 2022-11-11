c2274be094f2af6e13941f5e8aef50a661dcd536
webcoderspeed <webcoderspeed@gmail.com>
2022-11-12 00:19:04 +0530

    terms and condition

---
+++ src/data/terms-and-conditions.ts
@@ -0,0 +1,147 @@
+const data: {
+  id: number;
+  title: string;
+  info: string[];
+  extraInfo?: {
+    title: string;
+    info: string[];
+  }[];
+}[] = [
+  {
+    id: 1,
+    title: 'Algemene',
+    info: [
+      'Zodra u de lidmaatschapsovereenkomst (verder te noemen Overeenkomst) tekent, wordt u lid (hierna; het “Lid”) van Vinci Club B.V. (Hierna; “Vinci Club”)',
+      'De lidmaatschapsacties hebben alleen betrekking op de 1- en 2 jarige lidmaatschapsvormen van Vinci Club',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Voorwaarden',
+    info: [
+      'Deze voorwaarden zijn van toepassing op alle diensten van Vinci Club en beheersen daarmee de rechtsverhouding tussen Vinci Club en het Lid.',
+      'Vinci Club is gerechtigd de op het lidmaatschap van toepassing zijnde voorwaarden te wijzigen. Vinci Club zal het lid hiervan tijdig op de hoogte brengen. De wijzigingen treden één kalendermaand na de bekendmaking in werking, tenzij een afwijkende wettelijke termijn is vereist, die alsdan wordt toegepast. Indien een Lid een voor hem nadelige wijziging niet wenst te accepteren, kan hij zijn lidmaatschap beëindigen met ingang van de datum waarop de nieuwe voorwaarden van kracht worden. De schriftelijke opzegging dient voor de ingangsdatum van de wijziging door Vinci Club te zijn ontvangen.',
+    ],
+  },
+  {
+    id: 3,
+    title: 'Lidmaatschap',
+    info: [
+      'Het Lid is bekend met het feit dat hij zijn lidmaatschap aangaat voor de afgesproken contractduur die ingaat vanaf de in de Overeenkomst vermelde ‘Ingangsdatum overeenkomst’',
+      'Het Lid, niet zijnde een rechtspersoon, heeft bij online inschrijving het recht om binnen 14 dagen na de dag van deze inschrijving aan Vinci Club mee te delen dat hij afziet van het lidmaatschap, dit zonder betaling van een boete en zonder opgaaf van reden. (Dit geldt niet voor andere vormen van inschrijving en/of indien er sprake is van een bedrijfslidmaatschap (hierna; “Bedrijfslidmaatschap”)). Deze opzegging dient te geschieden conform hetgeen bepaald is in artikel 7.2 van deze algemene voorwaarden. Indien de 14de dag in het weekend valt of op een nationale feestdag, loopt de termijn van 14 dagen tot en met het einde van de eerst daaropvolgende werkdag. Indien het Lid binnen de bedenktijd van 14 dagen besluit af te zien van het lidmaatschap, is Vinci Club gerechtigd om voor het aantal dagen dat het Lid gebruik heeft kunnen maken van de faciliteiten van Vinci Club, kosten in rekening te brengen (inschrijfgeld en kosten voor de dagen dat reeds gebruik kon worden gemaakt van de faciliteiten van Vinci Club).',
+      'Het lidmaatschap is strikt persoonlijk en kan alleen na toestemming van Vinci Club worden overgedragen.',
+      'Vinci Club behoudt zich het recht voor nieuwe leden bij inschrijving om identificatie te vragen.',
+      'Vinci Club zal gerechtigd zijn om op eender welk tijdstip identificatie van een Lid te vragen teneinde na te gaan of de persoon die met de lidmaatschapstag binnenkomt inderdaad het Lid betreft.',
+      'Het Lid mag gedurende de openingstijden trainen in de in de Overeenkomst vermelde club van Vinci Club.',
+      'Bij het Gezinsabonnement moet je kunnen aantonen dat je woonachtig bent op één en hetzelfde woonadres.',
+    ],
+  },
+  {
+    id: 4,
+    title: 'Lidmaatschapsgelden',
+    info: [
+      'Het lidmaatschapsgeld dient door het Lid via SEPA automatische periodieke incasso of contant vooruit voor de gehele contractperiode bij vooruitbetaling te worden voldaan, op 4 wekelijkse basis, dan wel desgewenst voor de initiële contractduur (of een verlenging daarvan).',
+      'Indien het lidmaatschap eerder wordt geactiveerd dan de ingangsdatum van de Overeenkomst en het Lid dus een aantal dagen eerder gebruik kan maken van de faciliteiten van Vinci Club, is het Lid over deze extra dagen een aanvullend lidmaatschapsgeld verschuldigd, te berekenen naar rato van het aantal extra dagen.',
+      ' De overeengekomen inschrijf- en administratiekosten dienen bij inschrijving op de club te worden voldaan. Het niet betalen van deze kosten betekent niet dat de Overeenkomst nietig verklaard kan worden.',
+      'Vinci Club behoudt zich het recht voor om achterstallen te innen via SEPA automatische incasso.',
+      'Bij niet tijdige betaling is het Lid zonder verdere ingebrekestelling in verzuim. Vanaf de datum waarop het Lid in verzuim is, worden alle voor de resterende duur van het lidmaatschap verschuldigde termijnen opeisbaar en dienen deze binnen 30 dagen na opeisbaarheid door het Lid aan Vinci Club te worden voldaan. Het Lid kan geen gebruik maken van de diensten en faciliteiten van Vinci Club tot het moment dat zijn vervallen schuld is voldaan. Vinci Club heeft het recht om haar verplichtingen op te schorten zolang het lid zijn verplichtingen niet nakomt. Nadat het lid aan zijn verplichtingen, te weten het betalen van alle openstaande termijnen tot einde lidmaatschap, heeft voldaan is Vinci Club gehouden haar verplichtingen uit de overeenkomst na te komen. Het lid wordt als dan in de gelegenheid gesteld gebruik te maken van de sportfaciliteiten van Vinci Club, waarbij de periode gelijkgesteld wordt aan de duur van de daadwerkelijke opschorting.',
+      'Indien Vinci Club over dient te gaan tot incasso van haar vordering op het Lid, is het Lid voorts de administratiekosten en (buitengerechtelijke) incassokosten verschuldigd ter grootte van de staffel incassokosten conform het percentage zoals bedoeld in de relevante incasso wet- en regelgeving, dan wel 15% van het openstaande bedrag, met een minimum van 40,- euro. Vanaf de datum waarop een bedrag opeisbaar was, kan Vinci Club het Lid 1% rente per maand in rekening brengen of de wettelijke rente indien die hoger is. Het Lid kan geen gebruik maken van de diensten en faciliteiten van Vinci Club tot het moment dat zijn vervallen schuld in overeenstemming met de afspraken met het incassobureau is c.q. zal worden voldaan.',
+      'Voor acties en/of speciale aanbiedingen zijn de voorwaarden na te vragen in de Vinci Club vestiging of op de website.',
+      'Aan acties en/of speciale aanbiedingen kunnen voor- of achteraf nimmer rechten worden ontleend.',
+      'De actiemaanden worden altijd voorafgaand aan het abonnement toegepast tenzij anders is aangegeven. Actieperiodes vallen buiten de contractduur.',
+      'Er kan in geen enkel geval aanspraak worden gemaakt op restitutie van de contributie of andere bedragen.',
+      'Alle prijzen zijn inclusief BTW. Vinci Club is te allen tijde gerechtigd om haar tarieven aan te passen in geval van wijzigingen in de hoogte van de BTW.',
+      'Vinci Club is bevoegd om op 1 januari van elk kalenderjaar de lidmaatschapsprijs te indexeren met maximaal 5%. Indien deze prijsverhoging wordt doorgevoerd gedurende de eerste 3 maanden vanaf de in de Overeenkomst vermelde ‘startdatum van het lidmaatschap’, dan heeft het Lid het recht om zijn lidmaatschap met onmiddellijke ingang te ontbinden.',
+    ],
+  },
+  {
+    id: 5,
+    title: 'Privacy',
+    info: [
+      'Vinci Club verwerkt persoonsgegevens van het Lid of een andere betrokkene op behoorlijke en zorgvuldige wijze en binnen de kaders van de toepasselijke wet- en regelgeving, waaronder in ieder geval begrepen de wet- en regelgeving op het gebied van gegevensbescherming, zoals de Algemene Verordening Gegevensbescherming (AVG)',
+      'In de Privacyverklaring van Vinci Club wordt op een rijtje gezet welke gegevens door Vinci Club worden verwerkt, voor welke doeleinden Vinci Club dat doet en hoe met de persoonsgegevens wordt omgegaan. Daarnaast wordt uitgelegd op welke wijze betrokkenen hun rechten kunnen uitoefenen op de verwerking van hun persoonsgegevens. De laatste versie van de Privacyverklaring vind je altijd op onze website.',
+    ],
+  },
+  {
+    id: 6,
+    title: 'Lidmaatschapstag',
+    info: [
+      'Het Lid ontvangt, na ondertekening van de Overeenkomst, een lidmaatschapstag. Deze tag blijft eigendom van Vinci Club en is strikt persoonlijk.',
+      'Het is alleen mogelijk om gebruik te maken van de club op vertoon van een geldige lidmaatschapstag.',
+      'Vinci Club is bevoegd om bij misbruik van deze tag het Lid de toegang te ontzeggen.',
+      'Verlies of diefstal van de lidmaatschapspas dient direct aan Vinci Club te worden gemeld. Vinci Club zal de kosten van 15,00 euro voor een nieuwe tag in rekening brengen bij het Lid.',
+    ],
+  },
+  {
+    id: 7,
+    title: 'Beëindiging of ontbinding lidmaatschap',
+    info: [
+      'Indien het Lid zijn lidmaatschap niet 4 weken voor de einddatum van de initiële contractduur schriftelijk bij de desbetreffende club opzegt, wordt het lidmaatschap na de contractduur automatisch verlengd voor onbepaalde duur en geldt een opzegtermijn van 4 weken.',
+      'De opzegging kan worden gedaan op dezelfde wijze als de Overeenkomst is afgesloten, dan wel naar keus van het Lid schriftelijk middels een het bij Vinci Club verkrijgbaar opzegformulier of middels een eigen opzegbrief. Een schriftelijke opzegging dient het Lid te richten aan Vinci Club B.V. , Cruyslei 54A, 2100 Deurne. In geval het Lid zich online heeft ingeschreven, kan het Lid ook online (via de website www.vinci-club.be) of per e-mail (info@vinci-club.be) opzeggen.',
+      'Om de goede ontvangst van de opzegbrief te waarborgen, adviseert Vinci Club deze per aangetekende post te verzenden.',
+      'Het, door het Lid, tussentijds beëindigen van de Overeenkomst is uitsluitend kosteloos mogelijk op medische gronden waarbij een medische verklaring of ander – ten genoegen van Vinci Club – deugdelijk bewijs vereist is, bij het verhuizen buiten een straal van vijftien kilometer van de club van Vinci Club, waarbij een bewijs van inschrijving in de nieuwe gemeente vereist is, krijgt het lid de mogelijkheid om tussentijds de overeenkomst te beëindigen, met inachtneming van een opzegtermijn van 28 dagen.',
+      'Vinci Club geeft een Looptijd Verkorting optie. Indien het lid besluit gebruik te maken van de Looptijd Verkorting optie zal Vinci Club de door het Lid verschuldigde lidmaatschapsprijs herberekenen op basis van de resterende periode (inclusief de betaalperiode opzegtermijn) vermenigvuldigd met het van toepassing zijnde vaste tarief van 17,50 euro per 4 weken. Een indicatieve berekening van de vergoeding voor Looptijd Verkorting kan worden opgevraagd aan de balie van in de club van Vinci Club. De uiteindelijke verschuldigde totale vergoeding voor Looptijd Verkorting zal worden vermeld in de opzeg bevestiging van Vinci Club. Voor de in dit artikel 7.4 en artikel 7.5 genoemde gevallen geldt een opzegtermijn van 4 weken.',
+    ],
+  },
+  {
+    id: 8,
+    title: 'Opschorting (tijdelijk stopzetten)',
+    info: [
+      'In geval van een langdurige ziekte, blessure, zwangerschap of letsel kan met schriftelijke toestemming van Vinci Club het lidmaatschap tijdelijk, tot een maximum van 6 periodes van 4 wekelijkse termijnen, worden opgeschort (dit geldt niet bij bijv. studie, stage, werk en vakantie). Opschorting is alleen mogelijk na het overleggen van een medische verklaring of ander – ten genoegen van Vinci Club – deugdelijk bewijs.',
+      'Opschorting van het lidmaatschap kan nooit met terugwerkende kracht.',
+      'De betalingsverplichting van het Lid wordt opgeschort voor de daadwerkelijke duur van de opschorting. De einddatum van de lidmaatschapsovereenkomst wordt verlengd met de duur van de daadwerkelijke opschorting.',
+    ],
+  },
+  {
+    id: 9,
+    title: 'Openingstijden',
+    info: [
+      'Vinci Club is bevoegd om de club geheel of gedeeltelijk te sluiten tijdens algemeen erkende feestdagen en schoolvakanties.',
+      'Vinci Club is bevoegd de openingstijden van (of onderdelen van) een club tijdelijk of blijvend te wijzigen.',
+      'Vinci Club is bevoegd de in de Overeenkomst vermelde club voor reparatie en onderhoudswerk te sluiten.',
+    ],
+  },
+  {
+    id: 10,
+    title: 'Klachten en Informatieverplichting',
+    info: [
+      'Veranderingen in de persoonlijke situatie van het Lid (o.a. adres- of bankgegevens) dienen direct, schriftelijk, aan de in de Overeenkomst vermelde club te worden doorgegeven.',
+      'Bij het niet (tijdig) doorgeven van deze wijzigingen en wanneer Vinci Club kosten dient te maken om de nieuwe (persoon)gegevens te achterhalen, zullen deze kosten in rekening worden gebracht aan het Lid.',
+      'In geval van klachten met betrekking tot Vinci Club dient het Lid zich in eerste instantie te wenden tot de servicemedewerker en in tweede instantie tot Vinci Club. Mail naar info@vinci-club.be',
+    ],
+  },
+  {
+    id: 11,
+    title: 'Personal Training',
+    info: [
+      'Vinci Club, dan wel derden, bieden in onze vestiging personal training aan. Personal training is niet bij het lidmaatschap inbegrepen.',
+    ],
+  },
+  {
+    id: 12,
+    title: 'Aansprakelijkheid',
+    info: [
+      'Vinci Club is niet aansprakelijk voor enig letsel of ongeval of zoekgeraakte voorwerpen van welke aard en door welke oorzaak dan ook, behoudens in geval van opzet of bewuste roekeloosheid zijdens Vinci Club of haar leidinggevenden.',
+      'Ieder Lid is zelf verantwoordelijke voor persoonlijke ongevallen en zijn persoonlijke bezittingen. Het Lid wordt geadviseerd om zich hiertoe treffend te verzekeren. Noch Vinci Club, noch zijn medewerkers kunnen verantwoordelijk worden gesteld voor het verlies, de beschadiging of diefstal van goederen op het grondgebied van de club.',
+    ],
+  },
+  {
+    id: 13,
+    title: 'Huisreglement',
+    info: [
+      'Het Lid is bekend met de door Vinci Club gehanteerde huisregels. Deze huisregels zijn aanwezig in de club en kunnen indien gewenst worden opgevraagd bij de receptie. Het Lid is verplicht deze huisregels na te leven.',
+      'De club kan te allen tijde de toegang weigeren of ontzeggen aan personen wiens gedrag daar aanleiding toe geeft.',
+    ],
+  },
+  {
+    id: 14,
+    title: 'Slotbepalingen',
+    info: [
+      'Op alle door Vinci Club gesloten lidmaatschapsovereenkomsten is het Nederlandse recht van toepassing.',
+      'Overeenkomsten tussen het Lid en Vinci Club kunnen alleen schriftelijk worden aangegaan en kunnen alleen schriftelijk worden gewijzigd.',
+      'Wanneer één van de hierboven staande bepalingen ongeldig of onwettig is, blijven de overige bepalingen gelden.',
+    ],
+  },
+];
+
+export default data;
--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import data from 'src/data/terms-and-conditions';
 
 const TermsAndConditions = () => {
   return (
-    <div>TermsAndConditions</div>
-  )
-}
+    <div className='p-8'>
+      <h1 className='text-4xl font-bold text-red-600 uppercase text-center'>
+        Terms and Conditions
+      </h1>
 
-export default TermsAndConditions
+      {data.map((item) => {
+        return (
+          <div
+            key={item.id}
+            className='flex flex-col gap-2 mt-4'
+          >
+            <h1 className='font-semibold'>
+              {item.id}. {item.title}
+            </h1>
+            <div className='px-4'>
+              {item.info.map((info, index) => {
+                return (
+                  <p key={index}>
+                    <span className='font-semibold'>
+                      {item.id}.{index + 1}.
+                    </span>
+                    {info}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TermsAndConditions;

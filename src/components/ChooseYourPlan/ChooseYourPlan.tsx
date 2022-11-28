88780091e16b163809429a657a8e327885487f51
webcoderspeed <webcoderspeed@gmail.com>
2022-11-28 19:35:09 +0530

    dumbell aligment fix

--- src/components/ChooseYourPlan/ChooseYourPlan.tsx
+++ src/components/ChooseYourPlan/ChooseYourPlan.tsx
@@ -9,12 +9,12 @@ import BodyImg from 'public/body.png';
 const ChooseYourPlan = () => {
   return (
     <div className='bg-plan py-16 p-4 md:p-20 w-full '>
-      <div className=' w-full flex justify-center flex-col md:flex-row lg:px-24'>
+      <div className=' w-full flex justify-center flex-col md:flex-row relative'>
         <div className='h-full '>
           <Image
             src={BarBellImg}
             alt='barbell'
-            className='max-w-[184px] hidden md:block -mt-16 lg:translate-x-8'
+            className='max-w-[156px] hidden md:block absolute md:-left-14 lg:-left-5 xl:left-24 -top-20 md:-top-18 lg:-top-16 xl:-top-14'
           />
         </div>
         <div className='flex-1 text-center w-full '>
@@ -33,18 +33,20 @@ const ChooseYourPlan = () => {
       </div>
       <div className='flex flex-wrap gap-5 justify-center items-center p-6'>
         {data.map((item) => (
-          <ChooseYourPlanCard key={item.id} {...item}/>
+          <ChooseYourPlanCard
+            key={item.id}
+            {...item}
+          />
         ))}
       </div>
       <div className='flex justify-end items-center'>
         <Image
-          src
-          ={BodyImg}
+          src={BodyImg}
           alt='body'
           className='max-w-[192px] hidden md:block  lg:-translate-x-40'
         />
-        </div>
-    </div>  
+      </div>
+    </div>
   );
 };
 

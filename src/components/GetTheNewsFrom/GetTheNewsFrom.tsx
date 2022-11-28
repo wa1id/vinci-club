f5def32aba83a0ebe6219d0c9aff128bf861bd1f
webcoderspeed <webcoderspeed@gmail.com>
2022-11-28 23:16:54 +0530

    added text stroke

--- src/components/GetTheNewsFrom/GetTheNewsFrom.tsx
+++ src/components/GetTheNewsFrom/GetTheNewsFrom.tsx
@@ -8,13 +8,13 @@ import { FiArrowUpRight } from 'react-icons/fi';
 
 const GetTheNewsFrom = () => {
   return (
-    <div className='py-16 p-4  w-full lg:md:p-28 bg-secondary'>
+    <div className='py-16 p-4 md:p-20 lg:p-28 w-full bg-secondary'>
       <div className='w-full flex justify-center flex-col-reverse md:flex-row  gap-5  items-center text-center'>
         <div className='w-full h-full relative'>
           <Image
             src={GetTheNewsFromDumbell}
             alt='dumbell'
-            className='max-w-[156px] hidden lg:block absolute top-0 left-0  -translate-x-16 -translate-y-24 md:-translate-x-20 md:-translate-y-28'
+            className='max-w-[156px] hidden lg:block absolute top-0 left-0  -translate-x-16 -translate-y-24 xl:-translate-x-20 md:-translate-y-28'
           />
           <Heading
             aboveHeading='Get the'
@@ -22,14 +22,20 @@ const GetTheNewsFrom = () => {
             headingClassName='text-primary '
             lineClassName='bg-white'
           />
-          <h3 className=' [text-shadow:_0px_13px_1px_rgb(0_0_0_/_0.4%)] uppercase text-4xl md:text-5xl lg:text-6xl font-drukwide '>
+          <h3
+            className=' [text-shadow:_0px_13px_1px_rgb(0_0_0_/_0.4%)] uppercase text-4xl md:text-5xl lg:text-6xl font-drukwide'
+            style={{
+              WebkitTextStroke: '1px ##1D1D1D',
+              WebkitTextFillColor: 'transparent',
+            }}
+          >
             VINCI CLUB
           </h3>
           <p className='text-primary font-normal m-auto text-xl mt-4 md:max-w-[308px]'>
             Stay up to date with recent news, advice and weekly offers
           </p>
 
-          <div className='w-full flex justify-center items-center flex-col gap-5 my-4'>
+          <div className='w-full flex justify-center items-center flex-col gap-5 my-4 p-6'>
             <input
               type='text'
               placeholder='Enter your email'
@@ -40,7 +46,7 @@ const GetTheNewsFrom = () => {
             </button>
           </div>
         </div>
-        <div className='w-full h-full flex items-center justify-center relative'>
+        <div className='w-full h-full flex items-center justify-center relative p-6'>
           <Image
             src={GetTheNewsFromGirlImg}
             alt='get the news from'

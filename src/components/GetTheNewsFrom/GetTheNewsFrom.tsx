ad794ddbe3d4fb7f84fe1c92d3b64d8adf087ba7
webcoderspeed <webcoderspeed@gmail.com>
2022-11-29 22:36:05 +0530

    subscribe newsletter

--- src/components/GetTheNewsFrom/GetTheNewsFrom.tsx
+++ src/components/GetTheNewsFrom/GetTheNewsFrom.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from '../Heading/Heading';
 import GetTheNewsFromGirlImg from 'public/get-the-news-from-girl.png';
 import GetTheNewsFromDumbell from 'public/get-the-news-from-dumbell.png';
 import GetTheNewsFromBoxingGloves from 'public/get-the-news-from-boxing-gloves.png';
 import Image from 'next/image';
 import { FiArrowUpRight } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const GetTheNewsFrom = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // subscribe to newsletter using mailchimp
+    e.preventDefault();
+
+    if (!email) {
+      toast.warn('Please enter your email address');
+      return;
+    }
+
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: 'subscribed',
+        },
+      ],
+    };
+
+    const options = {
+      method: 'POST',
+      url: `https://us1.api.mailchimp.com/3.0/lists/72aafb93a7`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'auth ca41313ee7983f687c4c6b2e2f1995ca-us14',
+      },
+      data,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        toast.success('You have successfully subscribed to our newsletter');
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Something went wrong, please try again');
+      });
+  };
+
   return (
     <div className='py-16 p-4 md:p-20 lg:p-28 w-full bg-secondary'>
       <div className='w-full flex justify-center flex-col-reverse lg:flex-row items-center text-center'>
@@ -38,17 +81,25 @@ const GetTheNewsFrom = () => {
             Stay up to date with recent news, advice and weekly offers
           </p>
 
-          <div className='w-full flex justify-center items-center flex-col gap-5 my-4 p-6'>
+          <form
+            className='w-full flex justify-center items-center flex-col gap-5 my-4 p-6'
+            onSubmit={handleSubmit}
+          >
             <input
-              type='text'
+              type='email'
               placeholder='Enter your email'
               className={'get-the-news-from-email-input'}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <button className='bg-primary text-white p-4 rounded-full ml-4 flex items-center justify-center gap-2 uppercase max-w-[300px]'>
+            <button
+              className='bg-primary text-white p-4 rounded-full ml-4 flex items-center justify-center gap-2 uppercase max-w-[300px]'
+              type='submit'
+            >
               {/* TODO: i18 */}
               Subscribe <FiArrowUpRight className='mt-1' />
             </button>
-          </div>
+          </form>
         </div>
         <div className='w-full h-full flex items-center justify-center relative p-6'>
           <Image

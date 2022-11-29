22718f5b305d8ac4efed6eb4c6573bfb600aea0b
webcoderspeed <webcoderspeed@gmail.com>
2022-11-29 21:37:00 +0530

    contact with vinci club

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Address from 'src/components/Address/Address';
+import ContactWithVinciClub from 'src/components/ContactWithVinciClub/ContactWithVinciClub';
 import ContactForm from 'src/components/form/ContactForm';
 
 const Contact = () => {
   return (
     <>
+      <ContactWithVinciClub />
       <Address />
       <ContactForm />
     </>

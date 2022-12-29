9a695bab1ca9e15ff0633fcaccfc91b080f6e7ee
webcoderspeed <webcoderspeed@gmail.com>
2022-12-29 09:16:30 +0530

    spelling fix

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -1,37 +1,35 @@
 import sgMail, { MailDataRequired } from '@sendgrid/mail';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+sgMail.setApiKey(process.env.SENDGRID_API as string);
 
 const signup = async (req: NextApiRequest, res: NextApiResponse) => {
-  // const {
-  //   firstName,
-  //   lastName,
-  //   email,
-  //   telephone,
-  //   interestedIn,
-  //   address,
-  //   referenceClub,
-  // } = req.body;
+  const {
+    firstName,
+    lastName,
+    email,
+    telephone,
+    interestedIn,
+    address,
+    referenceClub,
+  } = req.body;
 
   let message = 'html_template';
 
   const mailOptions: MailDataRequired = {
     from: {
-      name: 'Vinci Club', email: 'email@gmail.com'
+      name: 'Vinci Club', email: 'info@vinci-club.be'
     },
-    to: 'email@gmail.com',
-    subject: 'subject',
+    to: email,
+    subject: 'Bedankt voor uw inschrijving bij Vinci Club',
     html: message,
     templateId: process.env.CLIENT_TEMPLATE_ID,
   };
 
-  console.log(JSON.stringify(mailOptions))
 
   try {
     await sgMail.send(mailOptions);
   } catch (error: any) {
-    console.log(error)
     return res.status(500).json({ error: error.message || error.toString() });
   }
 };

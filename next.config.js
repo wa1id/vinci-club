cadf9635bf75d10b467f1898a87e1cf33687c926
webcoderspeed <webcoderspeed@gmail.com>
2023-01-08 15:14:25 +0530

    husky integration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-const { i18n } = require("./next-i18next.config");
+const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    USER_EMAILJS_SERVICE_ID: process.env.USER_EMAILJS_SERVICE_ID,
-    USER_EMAILJS_TEMPLATE_ID: process.env.USER_EMAILJS_TEMPLATE_ID,
-    USER_EMAILJS_PUBLIC_KEY: process.env.USER_EMAILJS_PUBLIC_KEY,
-  },
-  i18n,
-};
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		USER_EMAILJS_SERVICE_ID: process.env.USER_EMAILJS_SERVICE_ID,
+		USER_EMAILJS_TEMPLATE_ID: process.env.USER_EMAILJS_TEMPLATE_ID,
+		USER_EMAILJS_PUBLIC_KEY: process.env.USER_EMAILJS_PUBLIC_KEY,
+	},
+	i18n,
+	eslint: {
+		dirs: ['src'],
+	},
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
